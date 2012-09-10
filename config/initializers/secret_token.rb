62815d70bedfd6bf093aba199c0c90e5e9436a23
Maxim Rykunov <maxim@rykunov.info>
2012-09-10 12:59:29 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypr::Application.config.secret_token = 'd1c122a5ac67a6599a9de55acbcf93eb64d2781919f69772601f4964601abeb24095063938dab77c7bc45f535330155866781627ddecf0f3828a4fe282398e26'
